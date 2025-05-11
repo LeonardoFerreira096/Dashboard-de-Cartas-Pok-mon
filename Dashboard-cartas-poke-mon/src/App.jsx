35026d150bfe19e3fdda15fc9e78b5217cc6ace2
leonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2025-05-10 23:28:41 -0300

    01738044 - Estruturando as pastas

---
+++ Dashboard-cartas-poke-mon/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import 'bo'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Olaaaaaa</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
