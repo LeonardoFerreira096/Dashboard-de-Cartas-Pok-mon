6c0eb88ac4d4812f32d88849ab37915f7eeaefa3
leonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2025-05-13 23:58:13 -0300

    criei a pasta componetes pronta para receber a organizacao

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './compenentes/Header'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <main>
+        <Header/>
+        <section>
+           
+        </section>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/compenentes/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h1>Cartas Salvas:</h1>
+            </div>
+            <input type="text" />
+            <button>Buscar</button>
+        </div>
+
+    )
+}
+
+export default Header
